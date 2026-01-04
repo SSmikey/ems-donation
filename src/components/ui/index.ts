ae2985c11a3aa329ba1c06f140b4ed1b60563d2b
SSmikey <Seangnon09@gmail.com>
2026-01-04 15:15:03 +0700

    set tailwind for web-app

---
+++ src/components/ui/index.ts
@@ -0,0 +1,8 @@
+export { Button, buttonVariants } from './button'
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from './card'
+export { Dialog, DialogPortal, DialogOverlay, DialogClose, DialogTrigger, DialogContent, DialogHeader, DialogFooter, DialogTitle, DialogDescription } from './dialog'
+export { Input } from './input'
+export { Label } from './label'
+export { Select, SelectGroup, SelectValue, SelectTrigger, SelectContent, SelectItem, SelectSeparator, SelectScrollUpButton, SelectScrollDownButton } from './select'
+export { Table, TableHeader, TableBody, TableFooter, TableHead, TableRow, TableCell } from './table'
+export { ToastProvider, ToastViewport, Toast, ToastTitle, ToastDescription, ToastClose, ToastAction, type ToastActionElement } from './toast'
